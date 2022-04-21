04d07bd0dbbeeca07db0b3e2b164f2a65767edb8
sombriks <sombriks@gmail.com>
2022-04-21 03:08:56 -0300

    work no null pointer issues

--- app/src/main/kotlin/works/on/my/machine/app/TodoItemConrtoller.kt
+++ app/src/main/kotlin/works/on/my/machine/app/TodoItemConrtoller.kt
@@ -5,4 +5,16 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TodoItemController(var repository: TodoItemRepository) {
 
+    fun add(item: TodoItem): TodoItem {
+        return null
+    }
+
+    fun update(id: Long, item: TodoItem): TodoItem {
+        return null
+    }
+
+    fun remove(id: Long): String {
+        return "ok"
+    }
+
 }
